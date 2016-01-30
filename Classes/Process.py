2dcdcec6bb4371e5225943b307e500229b1df341
joseEnrique <joseenriqueruiznavarro@gmail.com>
2016-01-30 16:38:51 +0100

    hash def

--- Classes/Process.py
+++ Classes/Process.py
@@ -8,26 +8,41 @@ class Process(object) :
         self.pid= pid
         self.port= port
         self.status =status
-        self.name = self.namePid(self.pid)
+        self.name = self.__namePid(self.pid)
 
 
 
     def getPid(self):
-
         return self.pid
+
+
     def getPort(self):
 
         return self.port
 
+
+
     def getStatus(self):
         return self.status
 
+
+
     def getName(self):
         return self.name
-    def namePid(self,pid):
+    def __namePid(self,pid):
         p = psutil.Process(pid)
         return p.name()
 
+    def __hash__(self):
+        if not self:
+            return 0 # empty
+        value = ord(self[0]) << 7
+        for char in self:
+            value = 1000003*value ^ ord(char)
+        value = value ^ len(self)
+        if value == -1:
+            value = -2
+        return value
 
 
     def __str__(self):
